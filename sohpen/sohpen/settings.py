58051b30f074ab0ce4e4cbdf0a3cfe8aff8afdcf
Manish Sharma <mks2064@gmail.com>
2017-06-25 16:26:22 +0545

    ch

--- sohpen/sohpen/settings.py
+++ sohpen/sohpen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4ji_2s2p#k_1!waodgg)c^0iy_9n@s3e*m6g9x&$y46u5q3dm7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sophen.org']
 
